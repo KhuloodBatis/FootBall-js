13fca02a91a6d41bbcb879e7ce7d0e00799d7a72
KhuloodBatis <k.batis@bimventures.com>
2022-08-07 09:52:13 +0300

    end

--- script.js
+++ script.js
@@ -1,176 +1,127 @@
 'use strict';
+/**
+ * The Complete JavaScript Course 15
+Data Structures, Modern Operators and Strings
+Coding Challenge #1
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game ('game' variable on
+next page). In this challenge we're gonna work with that data.
+Your tasks:
+1. Create one player array for each team (variables 'players1' and
+'players2')
+2. The first player in any player array is the goalkeeper and the others are field
+players. For Bayern Munich (team 1) create one variable ('gk') with the
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+field players
+3. Create an array 'allPlayers' containing all players of both teams (22
+players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+new array ('players1Final') containing all the original team1 players plus
+'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called
+'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
+number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which
+team is more likely to win, without using an if/else statement or the ternary
+operator.
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+Then, call the function again with players from game.scored
+GOOD LUCK 😀
+ */
+//!The Complete JavaScript Course 16
+const game = {
+team1: 'Bayern Munich',
+team2: 'Borrussia Dortmund',
+players: [
+[
+'Neuer',
+'Pavard',
+'Martinez',
+'Alaba',
+'Davies',
+'Kimmich',
+'Goretzka',
+'Coman',
+'Muller',
+'Gnarby',
+'Lewandowski',
+],
+[
+'Burki',
+'Schulz',
+'Hummels',
+'Akanji',
+'Hakimi',
+'Weigl',
+'Witsel',
+'Hazard',
+'Brandt',
+'Sancho',
+'Gotze',
+],
+],
+score: '4:0',
+scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+'Hummels'],
+date: 'Nov 9th, 2037',
+odds: {
+team1: 1.33,
+x: 3.25,
+team2: 6.5,
+},
+};
 
-// Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+//!1)
+const [players1 , players2] = game.players;
+console.log(players1 , players2);
+//!2
+// const fieldPlayers = [...game.players[0]];
+// const players1 = [`gk:${fieldPlayers}`];
+// console.log(players1);
 
-// Data needed for first part of the section
-const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  
-    openingHours: {
-      thu: {
-        open: 12,
-        close: 22,
-      },
-      fri: {
-        open: 11,
-        close: 23,
-      },
-      sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-      },
-    },
-    order : function(starterIndex, mainIndex){
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    },
-
-     //? to do orderDelivery 
-
-   orderDelivery: function ({starterIndex =1 , mainIndex=0,time= '20:00' , address}){
-    console.log(
-        `Order recived! ${this.starterMenu[starterIndex]} 
-        and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time} `
-    );
-   },
-
-   orderPasta: function(ing1,ing2,ing3){
-    console.log(`Here is your declicious pasta with${ing1},${ing2},${ing3}`);
-   }
-  };
-   
-  // restaurant.orderDelivery({
-  //   time: '23:30',
-  //   address: ' Via del Sole, 21',
-  //   mainIndex: 2,
-  //   starterIndex: 2,
-  // });
-
-  // restaurant.orderDelivery({
-  //   address: ' Via del Sole, 21',
-  //   starterIndex: 1,
-  // });
-
-
-
-//   const { name , openingHours , categories}  = restaurant;
-//   console.log(name,openingHours,categories);
-
-//   const{name: restaurantName , openingHours: hours,
-// categories: tags } = restaurant;
-
-// console.log(restaurantName,hours,tags);
-// //! default value
-// const { menu = [], starterMenu: starters = []}= restaurant;
-// console.log(menu,starters);
-
-//! Mutating variables
-
-// let a = 111;
-// let b = 999;
-// const obj = { a:23 , b:7 , c:14 };
-// ( { a, b } = obj);
-
-// console.log(a , b);
-
-//! nestad Object
-// ? 1)
-// const {fri} = openingHours;
-// console.log(fri);
-//?2)
-// const {fri:{open,close}} = openingHours;
-// console.log(open,close);
-//?3)
-// const {fri:{open:o , close:c}} = openingHours;
-// console.log(o,c);
-
-//   const arr = [2,3,4];
-//   const a = arr[0];
-//   const b = arr[1];
-//   const c = arr[2];
-
-//   const [x,y,z] = arr ;
-//   console.log(x,y,z);
-//   console.log(arr);
- 
-//   const [first,, second]= restaurant.categories;
-//   console.log(first, second);
-
-// let [main,, scondary]= restaurant.categories;
-//   console.log(main, scondary);
-
-//   const temp = main;
-//   main = scondary;
-//   secodary = temp;
-//   console.log(main,scondary);
-
-// [main,scondary] = [scondary,main];
-// console.log(main,scondary);
+// const fieldPlayers2 = [...game.players[1]];
+// const players2 = [`gk:${fieldPlayers2}`];
+// console.log(players2);
+const [gk,...fieldPlayers] = players1;
+console.log(gk,fieldPlayers);
 
 
-// const [starter,main] = restaurant.order(2,0)
-// console.log(starter,main);
+//!3
+// const allPlayers = players1.concat(players2);
+// console.log(allPlayers);
 
-// const nested = [2,4 ,[5,6]]
-// const [i,,j] = nested 
-// console.log(i,j);
+const allPlayers = [...players1,...players2];
+console.log(allPlayers);
+//!4 
+const players1Final = [...players1,'Thiago', 'Coutinho', 'Perisic']
 
-// const [i,,[j,k]]= nested
-// console.log(i,j,k);
+console.log(players1Final);
 
-// const [p=1, q=1 , r=1] = [8,9];
-// const [p=1, q=1 , r=1] = [8];
-// console.log(p,q,r)
+//!5 
 
-//!108
-// restaurant.nameGuests = 0;
-// const guests = restaurant.nameGuests || 10 ;
-// console.log(guests);
+const {odds:{team1, x:draw , team2}} = game;
+console.log(team1,draw,team2);
 
-// //? Nullish: null and undefined (NOT 0 or '')
-// // const guestCorrect = restaurant.nameGuests ?? 10;
-// // console.log(guestCorrect);
-// //!105
-// const arr = [7,8,9];
-// const badNewArr = [1,2, arr[0] , arr[1] , arr[2]];
-// console.log(badNewArr);
+//!6
+/**
+ * 6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
+number of goals that were scored in total (number of player names passed in)
+ */
 
-// const newArr = [1,2, ...arr];
-// console.log(newArr);
-// console.log(...newArr);
+const printGoals = function(...players){
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+}
 
-// const newMenu  = [...restaurant.mainMenu, 'Gnocci'];
-// console.log(...newMenu);
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
 
-// //jin 2 arrays
+printGoals(...game.scored)
 
-// const menu = [...restaurant.starterMenu , ...restaurant.mainMenu]
-
-// console.log(...menu);
-// //!! Itarables : arrays, string, maps,sets , Not objects
-
-// const str = 'jonas';
-// const letters = [...str, ' ', 's.'];
-// console.log(letters);
-// // console.log(`${...str}`);
-
-// const ing = [
-//   prompt('Let\'s mke pasta!?'),
-//   prompt('Ingredient 2?'),
-// prompt('Ing3')]
-// console.log(ing);
-
-// restaurant.orderPasta(ing[0],ing[1],ing[2])
-// restaurant.orderPasta(...ing);
-
-//!Use Any data type , return any data type, Short_circuiting 
-
-console.log(3&& 'Jonas');
-console.log('' && 'Jonas');
-console.log(true&&0);
-console.log(undefined&&null);
+//!7)
+//? here we use the logical no if 
+team1>team2 && console.log("team 1 is winner ");
+team1<team2 && console.log("team 2 is winner ");
